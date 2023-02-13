c221a85f810f59c61af5c8ec81bcaf6b958a771a
trcrsired <uwgghhbcad@gmail.com>
2023-02-13 04:26:38 -0500

    fix zhCN.lua

--- Options/locale/zhCN.lua
+++ Options/locale/zhCN.lua
@@ -1,3 +1,3 @@
-local L = AceLocale:NewLocale("LookingForGroup", "zhCN") or AceLocale:NewLocale("LookingForGroup", "zhTW")
+local L = AceLocale:NewLocale("MusicBox_Options", "zhCN") or AceLocale:NewLocale("MusicBox_Options", "zhTW")
 if not L then return end
 --@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
