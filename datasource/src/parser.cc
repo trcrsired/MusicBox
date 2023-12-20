e15f7ec5316b7ead22e255247284db16759b6dc5
trcrsired <uwgghhbcad@gmail.com>
2023-12-19 22:14:35 -0500

    parse

--- datasource/src/parser.cc
+++ datasource/src/parser.cc
@@ -131,38 +131,17 @@ print(tablelua,"}\n");
 	}
 
 	{
-	::fast_io::obuf_file tablelua(at(retaildir),u8"ZoneSoundKitIDToSoundEntryID.lua");
-	::csv::CSVReader tablecsv("SoundKitEntry.csv");
+	::fast_io::obuf_file tablelua(at(retaildir),u8"ZoneMusic.lua");
+	::csv::CSVReader tablecsv("ZoneMusic.csv");
 	print(tablelua,R"(local MusicBox = LibStub("AceAddon-3.0"):GetAddon("MusicBox")
-MusicBox.ZoneSoundKitIDToSoundEntryID=
+MusicBox.ZoneMusic=
 {)");
 	::std::map<::std::uint_least64_t,::fast_io::vector<::std::uint_least64_t>> mp;
 	for(auto const & row : tablecsv)
 	{
-		auto soundkitid = row["SoundKitID"].get<::std::uint_least64_t>();
-		if(zonesoundkits.contains(soundkitid))
-		{
-			auto filedataid = row["FileDataID"].get<::std::uint_least64_t>();
-			mp[soundkitid].push_back(filedataid);
-		}
-	}
-
-	for(auto const & e : mp)
-	{
-		print(tablelua,"[",e.first,"]=");
-		if(e.second.size()==1)
-		{
-			print(tablelua,e.second.front(),",\n");
-		}
-		else
-		{
-			print(tablelua,"{");
-			for(auto const &e1 : e.second)
-			{
-				print(tablelua,e1,",");
-			}
-			print(tablelua,"},\n");
-		}
+		print(tablelua,"[",row["ID"].get_sv(),"]={\"",
+		row["Sounds_0"].get_sv(),",",
+		row["Sounds_1"].get_sv(),"},\n");
 	}
 print(tablelua,"}\n");
 	}
